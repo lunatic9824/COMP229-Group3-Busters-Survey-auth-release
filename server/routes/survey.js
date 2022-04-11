664227dc27bfd12a8033f831f37c40016f3eb96e
pankaj sharma <lunaticfringe9824@gmail.com>
2022-04-11 14:33:28 +0530

    Auth

--- server/routes/survey.js
+++ server/routes/survey.js
@@ -3,6 +3,15 @@ Group project
 Comp 229 Sec003
 Winter 2022
 */
+function requireAuth(req,res,next)
+{
+    //check if the user is logged in
+    if(!req.isAuthenticated())
+    {
+        return res.redirect('/login');
+    }
+    next();
+}
 
 var express = require('express');
 var router = express.Router();
@@ -10,21 +19,21 @@ var router = express.Router();
 let surveyController = require('../controllers/survey');
 
 // Router for lists movies function
-router.get('/list', surveyController.surveyList);
+router.get('/list',requireAuth, surveyController.surveyList);
 
 // Router for movie details function
-router.get('/details/:id', surveyController.details);
+router.get('/details/:id',requireAuth, surveyController.details);
 
 // Routers for edit functions
-router.get('/edit/:id', surveyController.displayEditPage);
-router.post('/edit/:id', surveyController.processEditPage);
+router.get('/edit/:id',requireAuth, surveyController.displayEditPage);
+router.post('/edit/:id',requireAuth, surveyController.processEditPage);
 
 // Router for Delete function
-router.get('/delete/:id', surveyController.performDelete);
+router.get('/delete/:id',requireAuth, surveyController.performDelete);
 
 // Routers for Add functions
-router.get('/add', surveyController.displayAddPage);
-router.post('/add', surveyController.processAddPage);
+router.get('/add',requireAuth, surveyController.displayAddPage);
+router.post('/add',requireAuth, surveyController.processAddPage);
 
 
 module.exports = router;
